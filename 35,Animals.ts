da712698b1594fd26134cceec69f7afddbf94f4c
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:53:42 -0700

    Add files via upload

---
+++ 35,Animals.ts
@@ -0,0 +1,5 @@
+let Animals :string []=['cat','dog','tiger'];
+for (let animal of Animals){
+console.log ('A $ {Animal} would make agreat pet');
+}
+console.log ('Any of ther animals would make agreat pet !');
