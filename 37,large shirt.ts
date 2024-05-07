1b3927242f981edf5b5a56eb5b44f7f00502d702
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:56:26 -0700

    Add files via upload

---
+++ 37,large shirt.ts
@@ -0,0 +1,6 @@
+function make_shirt (size : string ='large',test:string = 'I love Typescript'){
+    console.log ('creating a $ {size} shirt with the message: $ {text');
+}
+make_shirt();
+make_shirt('Meadium');
+make_shirt('small,I love pthon');
