28d68fea46e4e91898f40549cd8345db1489a937
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 23:02:43 -0700

    Add files via upload

---
+++ 41,magicians.ts
@@ -0,0 +1,7 @@
+let magician :string[]=['Harry potter','Ron weasley','Hermone Granger','Albus Dumbledore'];
+function show_magicians (magician:string []){
+    magician.forEach (element =>{
+        console.log (element);
+    });
+}
+show_magicians(magician);
