936623cf9244449890eb7a2572d5937bc85078b9
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:57:53 -0700

    Add files via upload

---
+++ 38,cities.ts
@@ -0,0 +1,6 @@
+function describe_city (city:string,country:string='Deafault country'){
+    console.log (' $ {city} in $ {country}');
+}
+describe_city ('karachi','pakistan');
+describe_city ('tokjo','japan');
+describe_city ('paris');
