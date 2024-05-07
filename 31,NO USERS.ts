c378fc3153a91ec921be1321b10e05ea727dc4c3
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:43:52 -0700

    Add files via upload

---
+++ 31,NO USERS.ts
@@ -0,0 +1,8 @@
+let UserNames :string []=['admin','aliu','arham','kamran','zeeshan'];
+if (UserNames.length ===0){
+    console.log ('we need to find some user !');
+}
+else{
+    UserNames =[];
+    console.log ('All user Names have been removed.' +UserNames.length)
+}
